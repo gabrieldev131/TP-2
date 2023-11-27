ce65002da5372239e7eb2590e04b867201eac1d4
gabrieldev131 <119118315+gabrieldev131@users.noreply.github.com>
2023-11-27 11:34:03 -0300

    logging.h

--- logging.h
+++ logging.h
@@ -14,7 +14,7 @@ typedef struct hospital Hospital;
 // Função para inicializar uma lista encadeada vazia
 Lista *inicia_Lista();
 
-// Função para adicionar um elemento ao final da lista encadeada
+// Função para adicionar um elemento no inicio da lista encadeada
 void insere_Lista(Lista *lista, void *dados);
 
 // Função que remove um valor da lista
@@ -29,6 +29,8 @@ void liberaLista(Lista *lista);
 
 // Função para inicializar uma fila vazia
 Fila *inicia_Fila();
+
+//append não foi usado, foi adaptada para uma função mais complexa que já insere ordenadamente chamada "insere_ordenado()" 
 
 // Função que remove um valor da fila
 void remove_dados_fila(Fila *fila);
@@ -71,28 +73,28 @@ int gerarIdade();
 Paciente *gerarPaciente(int ID);
 
 //coloca na fila do raiox
-void fila_atendimento_raiox(Fila *fila_SE, Fila *raiox, Hospital *h, int i);
+void fila_atendimento_raiox(Fila *fila_SE, Fila *raiox, Hospital *hospital, int unidade_de_tempo);
 
 //função que atualiza o tempo para terminar o raio x
-void fila_atendimento_laudo(Fila *fila_raiox, Fila *laudo, Hospital *h, int i);
+void fila_atendimento_laudo(Fila *fila_raiox, Fila *laudo, Hospital *hospital, int unidade_de_tempo);
 
-//função que atualiza o tempo para terminar o raio x
-void atualiza_tempo_raiox(Hospital *h);
+//função que atualiza o tempo para terminar o raio x e verifica se o exame terminou
+void atualiza_tempo_raiox(Hospital *hospital);
 
-//função que atualiza o tempo para terminar o laudo
-void atualiza_tempo_laudo(Hospital *h);
+//função que atualiza o tempo para terminar o laudo e verifica se terminou o atendimento
+void atualiza_tempo_laudo(Hospital *hospital);
 
-//função que atualiza o raio x caso o tempo seje 0
-void atualiza_raiox(Hospital *h, Fila *fila_raiox, int j);
+//função que atualiza o tempo de saida do paciente no raio-x e o tira do raio-x
+void atualiza_raiox(Hospital *hospital, Fila *fila_raiox, int unidade_de_tempo);
 
-//função que atualiza o laudo caso o tempo seje 0
-void atualiza_laudo(Hospital *h, Fila *fila_laudo, int j);
+//função que atualiza o tempo de saida do paciente da sala de laudo e o tira da sala de laudo
+void atualiza_laudo(Hospital *hospital, Fila *fila_laudo, int unidade_de_tempo);
 
 //função que verifica se tem máquina de raio x disponivel
-int verifica_raiox(Hospital *h);
+int verifica_raiox(Hospital *hospital);
 
 //função que verifica se tem médico disponivel e se o paciente já passou no raio x
-int verifica_laudo(Hospital *h);
+int verifica_laudo(Hospital *hospital);
 
 //função que atualiza a contagem de tempo dos pacientes
 void atualiza_lista_controle(Fila *fila_laudo, Lista *lista_controle);
@@ -101,13 +103,13 @@ void atualiza_lista_controle(Fila *fila_laudo, Lista *lista_controle);
 float media_laudo(Lista *controle);
 
 //pega a média de tempo para se ter o laudo por patologia
-void media_patologias(Lista *controle, Hospital *h);
+void media_patologias(Lista *controle, Hospital *hospital);
 
 //função que retorna o número de exames que demoraram mais de 7200U.T(Unidades de Tempo)
-int exames_apos_tempo(Lista *controle, int unidades_de_tempo);
+int exames_apos_tempo(Lista *controle);
 
 //função que printa as métricas
-void printa_metrica(Lista *controle, Hospital *h, int j);
+void printa_metrica(Lista *controle, Hospital *hospital);
 
 // Função para inserir paciente ordenadamente na fila pela gravidade
 void insere_ordenado(Fila *lista, Paciente *novo_paciente);
