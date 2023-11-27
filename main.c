a68fcbb68942187cdaba54e78d05630cbec475e7
gabrieldev131 <119118315+gabrieldev131@users.noreply.github.com>
2023-11-26 22:39:49 -0300

    main.c

--- main.c
+++ main.c
@@ -3,55 +3,52 @@
 #include <string.h>
 #include <time.h>
 #include "logging.h"
-
-#ifdef _WIN32 || _WIN64
-    #include <Windows.h>
-#else
-    #include <unistd.h>
-#endif
+#include <unistd.h>
 
 int main() {
-  Lista *lista_controle = init_Lista(); //lista não ordenada
-  Fila *fila_SE = init_Fila(); //fila da sala de espera (já ordenada)
-  Fila *fila_raiox = init_Fila();
-  Fila *fila_laudo = init_Fila();
-  Hospital *h;
-  int registro_pat[5][1];
+  Lista *lista_controle = inicia_Lista(); //lista não ordenada
+  Fila *fila_SE = inicia_Fila(); //fila da sala de espera (já ordenada)
+  Fila *fila_raiox = inicia_Fila();//fila de quem vai fazer/está fazendo raiox
+  Fila *fila_laudo = inicia_Fila();//fila de quem vai fazer/está fazendo raiox
+  Hospital *hospital = inicia_Hospital();//váriavel que controla os equipamentos e funcionários do hospital
   srand(time(NULL));
-  for(register int i = 1;i<43201;i++){
-    if(i&10 == 0){
-      sleep(2);
-      printa_metrica(lista_controle, registro_pat);
-    }
-    if(prob_pac()){  
-      Paciente *paciente = gerarPaciente(i);
-      append(lista_controle, paciente);
+  
+  for(int Unidade_de_tempo = 1;Unidade_de_tempo<43200;Unidade_de_tempo++){
+    if(probabilidade_paciente()){  
+      Paciente *paciente = gerarPaciente(Unidade_de_tempo);
+      insere_Lista(lista_controle, paciente);
+
       //coloca já ordenado na fila, por ordem de gravidade da doença
       insere_ordenado(fila_SE, paciente);
-
-    //sequência de verificações para olhar o tempo da consulta e as máquinas desocupadas
-    atualiza_tempo_raiox(h);
-    atualiza_tempo_laudo(h);
-    verifica_raiox(h);
-    verifica_laudo(h); 
-    if(verica_raiox(h) == 1){
-      //se tiver vaga no raiox, entra
-      fila_atendimento_raiox(fila_SE, fila_raiox, h, i);
-      if(verifica_laudo(h) == 1){
-        //depois do raio x, entra pra ver o laudo, se tiver vaga
-        fila_atendimento_laudo(fila_raiox, fila_laudo, h, i);
-      }
     }
-    void atualiza_lista_controle(fila_laudo, lista_controle);
+    //sequência de verificações para olhar o tempo da consulta e as máquinas desocupadas
+    atualiza_tempo_raiox(hospital);
+    atualiza_tempo_laudo(hospital);
+    atualiza_raiox(hospital, fila_raiox, Unidade_de_tempo);
+    atualiza_laudo(hospital, fila_laudo, Unidade_de_tempo);
+
+    if(verifica_raiox(hospital)){
+      //se tiver vagas no raiox, entra no raiox
+      fila_atendimento_raiox(fila_SE, fila_raiox, hospital, Unidade_de_tempo);
+    }
+    if(verifica_laudo(hospital)){
+      //se tiver feito raiox e o consultório de consulta estiver vazio, entra no consutório
+      fila_atendimento_laudo(fila_raiox, fila_laudo, hospital, Unidade_de_tempo);
+    }
+    //atualiza a lista que controla em que momento o paciente entrou ou saiu de alguma ala do hospital
+    atualiza_lista_controle(fila_laudo, lista_controle);
+    if((Unidade_de_tempo%10) == 0){
+      sleep(1);
+      //função que printam as médias de tempo
+      printa_metrica(lista_controle, hospital, Unidade_de_tempo);
     }
   }
-  printf("%d\n", size(lista_controle));
-  //print_list(lista_controle);
-  //print_fila(fila_SE);
-  freeLista(lista_controle);
-  freeFila(fila_raiox);
-  freeFila(fila_laudo);
-  freeFila(fila_SE);
-  free(h);
+
+  //liberando memória
+  liberaLista(lista_controle);
+  liberaFila(fila_raiox);
+  liberaFila(fila_laudo);
+  liberaFila(fila_SE);
+  liberaHospital(hospital);
   return 0;
 }
